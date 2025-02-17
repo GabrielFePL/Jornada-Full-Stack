700c0f40a0f5fdfec0d3e1f8f6e75b60e2ec994b
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-02-17 17:56:19 -0300

    Hot fix deploy.

--- Front-End/api/api.js
+++ Front-End/api/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-import "dotenv/config";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "https://jornada-full-stack.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
